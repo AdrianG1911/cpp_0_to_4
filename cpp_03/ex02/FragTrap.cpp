39e12526331d02f5ab7785fbc087b50e41e90567
Adrian Gutierrez Valero <adrgutie@c3r6s5.42tokyo.jp>
2025-04-13 22:11:32 +0900

    woww

--- cpp_03/ex02/FragTrap.cpp
+++ cpp_03/ex02/FragTrap.cpp
@@ -6,6 +6,12 @@ FragTrap::FragTrap()
 	std::cout << "FragTrap created\n";
 }
 
-FragTrap::FragTrap(const FragTrap &other)
+FragTrap::FragTrap(const FragTrap &other) : ClapTrap(other)
 {
+	std::cout << "FragTrap copied\n";
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &rhs)
+{
+	if (this != &rhs)
 }
